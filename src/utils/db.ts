500e1bf2184a73ccdcd071c34b6e19aa19a250b8
Akash <ag308669@gmail.com>
2023-10-28 01:00:08 +0530

    Added error handler

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -7,7 +7,13 @@ export const connectDb = async () => {
     await mongoose.connect(dbUri);
     logger.info('Successfully connected to the database');
   } catch (error) {
+    // This is only the initial connection error
     logger.error(error);
     process.exit(1);
   }
+
+  // This is the event listener for a connection error after the initial one
+  mongoose.connection.on('error', (err) => {
+    logger.error(err, 'MongoDB error');
+  });
 };
